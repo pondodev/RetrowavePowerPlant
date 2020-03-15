9731c34968983cb791e108068f9029c00b0f84d0
hooviegames <hoovler_bri@yahoo.com>
2020-03-14 17:15:00 -0700

    Added power plant interior, updated power plant exterior, trees, rocks, power line

---
+++ Assets/Scripts/EntryTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntryTrigger : MonoBehaviour
+{
+
+    public GameObject exterior;
+    public GameObject interior;
+    public GameObject ground;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (exterior.activeSelf)
+            {
+                exterior.SetActive(false);
+                ground.SetActive(false);
+                interior.SetActive(true);
+            }
+            else
+            {
+                exterior.SetActive(true);
+                ground.SetActive(true);
+                interior.SetActive(false);
+            }
+        }
+    }
+}
